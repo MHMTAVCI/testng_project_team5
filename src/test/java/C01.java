f48b9a45ede7dfb7ba2d2c934950c4905b8a1b35
Mehmet Avcı <mehmetavci1206@gmail.com>
2023-02-01 15:59:26 +0300

    v2

--- src/test/java/C01.java
+++ src/test/java/C01.java
@@ -2,5 +2,10 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("Localden Merhaba");
         System.out.println("Mehmet ilk işlem");
+
+
+
+
+        System.out.println("Mehmet ikinci işlem");
     }
 }
